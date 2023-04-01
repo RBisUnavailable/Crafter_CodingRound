b3819fb239a613143b88db8a414d1c1fb179fc54
Rachit Bajpai <92720186+RBisUnavailable@users.noreply.github.com>
2023-04-01 21:42:07 +0530

    Updated the required function

--- total_revenue.py
+++ total_revenue.py
@@ -9,3 +9,8 @@ def calculate_total_revenue(csv_file_name):
             sales_value = float(row[1])
             total_revenue += sales_value
     return total_revenue
+
+if __name__ == '__main__':
+    csv_file_name = 'sales.csv'
+    total_revenue = calculate_total_revenue(csv_file_name)
+    print('Total revenue:', total_revenue)
